0e81c5a12e5ee040d0c1d3df847bf05a21262690
paulohwebdev <nemeth.paulo@outlook.com>
2024-11-04 17:52:46 -0300

    aula10 e atividade

---
+++ aula-10/atividade.js
@@ -0,0 +1,14 @@
+let x = 7;
+let y = 8;
+
+// Soma
+console.log("A soma é:", x + y);
+
+// Diferença
+console.log("A diferença é:", x - y);
+
+// Multiplicação
+console.log("A multiplicação é:", x * y);
+
+// Divisão
+console.log("A divisão é:", x / y);
---
+++ aula-10/script.js
@@ -0,0 +1,118 @@
+// console.log("hello word 2")
+
+// let nome;
+// let idade;
+// let nota;
+
+// nome= "paulo henrique";
+// idade=22;
+// nota=8.2;
+
+// console.log(nome,idade,nota);
+// //let goiaba;
+// let goiaba;
+
+// let x = 7;
+// let y = 8;
+
+// // Soma
+// console.log("A soma é:", x + y);
+
+// // Diferença
+// console.log("A diferença é:", x - y);
+
+// // Multiplicação
+// console.log("A multiplicação é:", x * y);
+
+// // Divisão
+// console.log("A divisão é:", x / y);
+
+// const maskCPF= "000.000.000-00";
+
+// let tipo="lorem";
+
+// console.log(typeof tipo);
+
+// tipo=34;
+// console.log(typeof tipo);
+
+// let a =5, b=6, c=3;
+// let delt= (b**2) + (4 * a * c);
+// console.log(delt);
+
+// let numero=7;
+// let numero += 5;
+
+// console.log(numero);
+
+// let a1= true;
+// let a2 = false;
+
+// console.log(a2);
+
+
+ a= 7;
+ b = 8;
+
+// Soma
+console.log("A soma é:", a + b); //15
+
+// Diferença
+console.log("A diferença é:", a - b); // -1
+
+// Multiplicação
+console.log("A multiplicação é:", a * b); // 56
+
+// Divisão
+console.log("A divisão é:", a / b); //0.875
+
+// Operadores de atribuição:  JavaScript
+
+let x= 10;
+
+// Adiciona 5
+x += 5;
+console.log("Após adicionar 5:", x);
+
+// Subtrai 3
+x -= 3;
+console.log("Após subtrair 3:", x);
+
+// Multiplica por 2
+x *= 2;
+console.log("Após multiplicar por 2:", x);
+
+// Divide por 4
+x /= 4;
+console.log("Após dividir por 4:", x);
+
+let str1 = "Olá";
+let str2 = "Mundo";
+
+// Verifica se são iguais
+console.log("As strings são iguais?", str1 == str2);
+
+// Verifica se são diferentes
+console.log("As strings são diferentes?", str1 != str2);
+
+let num1 = 15;
+let num2 = 10;
+
+// Verifica se o primeiro é maior que o segundo
+console.log("O primeiro número é maior?", num1 > num2);
+
+// Verifica se o primeiro é menor ou igual ao segundo
+console.log("O primeiro número é menor ou igual?", num1 <= num2);
+
+let a = true;
+let b = false;
+let c = true;
+
+// Conjunção (todos devem ser verdadeiros)
+console.log("conjunção", a && b && c);
+
+// Disjunção (pelo menos um deve ser verdadeiro)
+console.log("Disjunção:", a || b || c);
+
+// Negação da disjunção
+console.log("Negação da disjunção:", !(a || b || c));
